f097f127683db51171a346d840a681e4a6f5850d
Sophia Cook <sophia.cook@codecentric.de>
2024-04-05 11:43:52 +0200

    move meta data to ssr and delete nextjs index page

--- src/ssr/app/index.tsx
+++ src/ssr/app/index.tsx
@@ -7,14 +7,17 @@ export const IndexPage: FC = () => {
       <link rel="stylesheet" href="/public/global.css"/>
       <link rel="stylesheet" href="/public/page.css"/>
       <title>HexSpace</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport"/>
+      <meta name="description" content="Sell your used space ships."/>
+      <link rel="icon" href="/favicon.ico"/>
     </head>
 
     <body>
     <main className="main">
       <h1>Welcome to Hexa Space Inc.</h1>
-          <p>Browse our our collection of fine <a href="/ships">space ships</a></p>
-        </main>
-      </body>
+      <p>Browse our our collection of fine <a href="/ships">space ships</a></p>
+    </main>
+    </body>
     </html>
   )
 }
