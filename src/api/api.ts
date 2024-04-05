f858eeeb5730067cfe4681d490c62bba490608a1
Sophia Cook <sophia.cook@codecentric.de>
2024-04-05 10:36:54 +0200

    setup first test route

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,1 +1,10 @@
-console.log("Test")
+import {Hono} from 'hono';
+import {serve} from '@hono/node-server';
+
+const app = new Hono()
+
+app.get('/test', (context)=>{
+    return context.text('Hello World')
+})
+
+serve({fetch: app.fetch, port: 8080})
