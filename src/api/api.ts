c04d9db4ddf1aed861e310deb5dcb94a472c6471
Magnus Schwarz <magnus.schwarz@codecentric.de>
2024-04-05 10:40:21 +0200

    setup inYard route

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,10 +1,11 @@
 import {Hono} from 'hono';
 import {serve} from '@hono/node-server';
+import ships from "@db/ships.json";
 
 const app = new Hono()
 
-app.get('/test', (context)=>{
-    return context.text('Hello World')
+app.get('/inYard', (context)=>{
+    return context.json(ships.inYard)
 })
 
 serve({fetch: app.fetch, port: 8080})
